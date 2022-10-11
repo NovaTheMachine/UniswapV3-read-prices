0c41fc1fd4e0c7abd306f7b1a20db41fdac2161b
NovaTheMachine <n.fdistri@gmail.com>
2022-10-11 02:01:36 +0200

    fetching prices

--- client/GetPricesUniswap.js
+++ client/GetPricesUniswap.js
@@ -10,13 +10,33 @@ const provider = new ethers.providers.JsonRpcProvider(
   "https://mainnet.infura.io/v3/ef99378ef61c441098c57e96c3074bd1"
 );
 
-async function getPrice() {
+async function getPrice(addressFrom, addressTo, amountInHuman) {
   const quoterAddress = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
   const quoterContract = new ethers.Contract(
     quoterAddress,
     QuoterABI,
     provider
   );
-  console.log(quoterContract)
+  const amountIn = ethers.utils.parseUnits(amountInHuman, 6);
+
+  const quoteAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+    addressFrom,
+    addressTo,
+    3000,
+    amountIn.toString(),
+    0
+  );
+  const amount = ethers.utils.formatUnits(quoteAmountOut.toString(),18)
+
+  return amount
 }
-getPrice()
+
+async function main() {
+  const addressFrom = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; //usdc
+  const addressTo = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const amountInHuman = "1297";
+
+  const amountOut = await getPrice(addressFrom, addressTo, amountInHuman);
+  console.log(amountOut);
+}
+main();
